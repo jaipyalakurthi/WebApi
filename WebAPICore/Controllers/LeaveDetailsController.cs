7bca285e20b3f42d511d6688bee5a9a46744228c
jai <jayanjai.0029@gmail.com>
2020-12-09 12:02:40 +0530

    pushing all the code

---
+++ WebAPICore/Controllers/LeaveDetailsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPICore.Data;
+using WebAPICore.Models;
+
+namespace WebAPICore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowOrigin")]
+    public class LeaveDetailsController : ControllerBase
+    {
+        private readonly EmployeeContext _context;
+
+        public LeaveDetailsController(EmployeeContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<LeaveDetails> GetLeaveDetails()
+        {
+            return _context.LeaveDetails;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostLeaveDetails([FromBody] LeaveDetails leaveDetails)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.LeaveDetails.Add(leaveDetails);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLeaveDetails", new { id = leaveDetails.LeaveID }, leaveDetails);
+        }
+    }
+}
